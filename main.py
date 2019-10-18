e500a8baef21a26327b28fe73afce10fe8b77647
HenrisonTao <henrisontao@gmail.com>
2019-10-19 01:28:13 +0800

    change style of parameter .

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
 import arch as trading
 
-MonitorBot= trading.OnTick("BTC/USD","5m",7)
+### Edit here
+trading_pair = "BTC/USD"
+timeframe = "5m"
+trace_back_days = 7
+###
+
+MonitorBot= trading.OnTick(trading_pair,timeframe,trace_back_days)
 MonitorBot.main_job()
